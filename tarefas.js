94b77e6cdc3b82eec2821a4f5949e9e518b329ce
Galantine <Galantine@users.noreply.github.com>
2021-07-16 00:32:54 -0300

    exercicio 1

--- tarefas.js
+++ tarefas.js
@@ -1,3 +1,4 @@
+let listaTarefas = document.querySelectorAll('#lista-tarefas')[0]
 
 class Tarefa {
   constructor(nome, categoria, realizada) {
@@ -5,10 +6,20 @@ class Tarefa {
     this.categoria = categoria;
     this.realizada = realizada;
   }
+
+  adicionaNaPagina(containerEl) {
+  	let novoItem = document.createElement('li')
+  	novoItem.classList.toggle('item-tarefa')
+  	novoItem.classList.toggle(`categoria-${this.categoria}`)
+  	novoItem.innerHTML = this.nome
+  	if(this.realizada)
+  		novoItem.classList.toggle('marcado')
+  	containerEl.appendChild(novoItem)
+  }
 }
 
-let nomesTarefas = ['Dever de casa', 'Jogar videogame', 'Comprar pão', 'Conversar com amigos pelo discord']
-let categoriasTarefas = ['estudos', 'lazer', 'compras', 'lazer']
+let nomesTarefas = ['Dever de casa', 'Jogar videogame', 'Comprar pão', 'Estudar para prova']
+let categoriasTarefas = ['estudos', 'lazer', 'compras', 'estudos']
 
 let tarefas = []
 
@@ -16,6 +27,11 @@ tarefas.push(new Tarefa('Comprar leite', 'compras', false))
 tarefas.push(new Tarefa('Escutar chimbinha', 'lazer', true))
 
 for(let i = 0; i < nomesTarefas.length; i++) {
-	tarefas.push(new Tarefa(nomesTarefas[i], categoriasTarefas[i], Math.floor(Math.random()*10)%2 == 0) )
-
+	tarefas.push(new Tarefa(nomesTarefas[i], categoriasTarefas[i], Math.floor(Math.random()*10)%2 == 0))
 }
+
+listaTarefas.innerHTML = ""
+
+tarefas.forEach(tarefa => {
+	tarefa.adicionaNaPagina(listaTarefas)
+})
